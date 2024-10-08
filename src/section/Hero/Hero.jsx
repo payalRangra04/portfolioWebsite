db94d13943417e32b625530cd5a203017881f065
payalrangra <144913589+payalRangra04@users.noreply.github.com>
2024-10-08 11:43:54 -0400

    Avatar

--- src/section/Hero/Hero.jsx
+++ src/section/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/avatar3.jpg'
+import heroImg from '../../assets/avatar3-cropped.jpg'
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import twitterLight from '../../assets/twitter-light.svg';
